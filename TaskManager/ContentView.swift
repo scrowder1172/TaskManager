47cb497f58f3ef74cc5590c045ecd45d4cec615a
Scott Crowder <scrowder@gmail.com>
2025-01-16 10:59:07 -0600

    Initial Commit

---
+++ TaskManager/ContentView.swift
@@ -0,0 +1,27 @@
+//
+// File: ContentView.swift
+// Project: TaskManager
+// 
+// Created by SCOTT CROWDER on 1/16/25.
+// 
+// Copyright © Playful Logic Studios, LLC 2025. All rights reserved.
+// 
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,20 @@
+//
+// File: TaskManagerApp.swift
+// Project: TaskManager
+// 
+// Created by SCOTT CROWDER on 1/16/25.
+// 
+// Copyright © Playful Logic Studios, LLC 2025. All rights reserved.
+// 
+
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
